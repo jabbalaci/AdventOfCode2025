4d6575af1c5f5b24af3cc129119956380616278f
Jabba Laci <jabba.laci@gmail.com>
2025-12-01 06:35:44 +0100

    day 1

---
+++ day01/d/part2/part2.d
@@ -0,0 +1,51 @@
+#!/usr/bin/env rdmd
+
+import std.conv;
+import std.file;
+import std.stdio;
+import std.string;
+
+void main()
+{
+    // const fname = "example.txt";
+    // const fname = "example2.txt";
+    const fname = "input.txt";
+
+    const lines = readText(fname).splitLines;
+    int value = 50;
+    int zeros = 0;
+
+    foreach (line; lines)
+    {
+        char dir = line[0];
+        int steps = line[1 .. $].to!int;
+        for (int i = 0; i < steps; ++i)
+        {
+            if (dir == 'L')
+            {
+                --value;
+                if (value < 0)
+                {
+                    value = 99;
+                }
+                if (value == 0)
+                {
+                    ++zeros;
+                }
+            }
+            else // if dir == 'R'
+            {
+                ++value;
+                if (value > 99)
+                {
+                    value = 0;
+                }
+                if (value == 0)
+                {
+                    ++zeros;
+                }
+            }
+        }
+    } // process lines
+    writeln(zeros);
+}
