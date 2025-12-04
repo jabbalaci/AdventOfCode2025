b3967f893ba0ba83657edc06a7e37c7556bdd859
Jabba Laci <jabba.laci@gmail.com>
2025-12-04 07:00:54 +0100

    Day 4

---
+++ day04/python/part2.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import sys
+from typing import NamedTuple
+
+import helper
+
+
+class GridPoint(NamedTuple):
+    row: int
+    col: int
+
+
+# ----------------------------------------------------------------------------
+
+char = str
+
+GridType = list[list[char]]
+
+
+# ----------------------------------------------------------------------------
+
+
+class Grid:
+    def __init__(self, fname: str) -> None:
+        self.matrix: GridType = [list(line) for line in helper.read_lines(fname)]
+
+    def get_cell_value(self, i: int, j: int, default=".") -> char:
+        if (i < 0) or (j < 0):
+            return default  # type: ignore
+        try:
+            return self.matrix[i][j]
+        except IndexError:
+            return default  # type: ignore
+
+    def get_all_neighbors(self, i: int, j: int) -> list[char]:
+        result = []
+        positions = [
+            (i - 1, j - 1),
+            (i - 1, j),
+            (i - 1, j + 1),
+            (i, j - 1),
+            (i, j + 1),
+            (i + 1, j - 1),
+            (i + 1, j),
+            (i + 1, j + 1),
+        ]
+        for a, b in positions:
+            result.append(self.get_cell_value(a, b))
+        return result
+
+    def analyze(self) -> list[GridPoint]:
+        result = []
+        for i, row in enumerate(self.matrix):
+            for j, c in enumerate(row):
+                if c == "@":
+                    neighbors: list[char] = self.get_all_neighbors(i, j)
+                    if neighbors.count("@") < 4:
+                        result.append(GridPoint(i, j))
+                    #
+                #
+            #
+        #
+        return result
+
+    def remove_papers(self, papers: list[GridPoint]) -> None:
+        for i, j in papers:
+            self.matrix[i][j] = "."
+
+    def start(self) -> None:
+        total = 0
+        while True:
+            papers = self.analyze()
+            if len(papers) == 0:
+                break
+            # else
+            self.remove_papers(papers)
+            total += len(papers)
+        #
+        print(total)
+
+    def show(self) -> None:
+        for i, row in enumerate(self.matrix):
+            for j, c in enumerate(row):
+                sys.stdout.write(c)
+            #
+            print()
+        #
+
+
+# ----------------------------------------------------------------------------
+
+
+def main() -> None:
+    # fname = "example.txt"
+    fname = "input.txt"
+
+    g = Grid(fname)
+    # g.show()
+    g.start()
+
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
