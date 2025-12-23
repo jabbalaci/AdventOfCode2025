58ade41ccf059112a85a55f79ca9c95af7e024df
Jabba Laci <jabba.laci@gmail.com>
2025-12-23 13:22:01 +0100

    Day 1 in Fortran

---
+++ day00_templates/fortran/part1.f90
@@ -0,0 +1,4 @@
+program part1
+  implicit none
+  print '(a)', "Part 1"
+end program part1
---
+++ day01/fortran/part1.f90
@@ -0,0 +1,45 @@
+program part1
+   implicit none
+   ! character(len=*), parameter :: fname = "example.txt"
+   character(len=*), parameter :: fname = "input.txt"
+   character(len=10) :: line
+   character :: direction
+   integer :: steps
+   integer :: unit, ios, i
+   integer :: value = 50
+   integer :: zeros = 0
+
+   ! Open the file
+   open(newunit=unit, file=fname, status="old", action="read", iostat=ios)
+   if (ios /= 0) then
+      print *, "Error: cannot open file"
+      stop 1
+   end if
+
+   ! Loop over lines
+   do
+      read(unit, '(a)', iostat=ios) line
+      if (ios /= 0) then
+         exit
+      end if
+      direction = line(1:1)
+      read(line(2:), *) steps
+      do i = 1, steps
+         if (direction == 'L') then
+            value = value - 1
+            if (value < 0) then
+               value = 99
+            end if
+         else if (direction == 'R') then
+            value = value + 1
+            if (value > 99) then
+               value = 0
+            end if
+         end if
+      end do
+      if (value == 0) then
+         zeros = zeros + 1
+      end if
+   end do
+   print '(i0)', zeros
+end program part1
